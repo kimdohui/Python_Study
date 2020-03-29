c33de0458b34e079c70978618f53224fdd709465
kimdohui <kdh24162416@gmail.com>
2020-03-29 23:48:20 +0900

    unit5 숫자 계산하기

---
+++ unit5/실수 계산하기.py
@@ -0,0 +1,32 @@
+print(3.5 + 2.1)
+print(4.3-2.5)
+
+# 컴퓨터가 실수를 표현할 때 오차가 발생하여 1.79999..이런 식으로 표현됨
+
+print(' ')
+print(' ')
+print(4.2+5)
+
+# 실수와 정수를 함께 계산하면 표현 범위가 넓은 실수로 계산됨
+
+print(' ')
+print(' ')
+print(float(5))
+print(float(1+1))
+print(float('5.3'))
+
+# 어떤 값을 실수로 만들고 싶다면 float(값) 형태로 써주면 됨
+
+print(' ')
+print(' ')
+print(type(3.5))
+
+# type에 실수를 넣으면 <class 'float'>가 나옴
+
+print(' ')
+print(' ')
+print(1.2+1.3j)
+print(complex(1.2, 1.3))
+
+# 파이썬에서는 복소수를 사용할 수 있는데 이때 허수부 뒤에 j를 붙인다.
+# 두 실수를 복소수로 사용할 때에는 complex를 사용하면 
